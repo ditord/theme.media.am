d8ab9cbb3a24f25b659d0db0e62d807ed49e621e
Robert <robert.papyan@matemat.io>
2025-02-10 15:10:29 +0400

    Featured post author bug fixed

--- templates/post-block-featured.php
+++ templates/post-block-featured.php
@@ -1,5 +1,5 @@
-<?php 
-$autor = wp_get_object_terms($post->ID, "author_posts"); 
+<?php
+$autor = wp_get_object_terms($post->ID, "author_posts");
 ?>
 
 <div class="post_block" data-category="<?php echo isset($cat->name) ? esc_attr($cat->name) : ''; ?>" data-url="<?php echo esc_url(get_permalink()); ?>">
@@ -15,21 +15,17 @@ $autor = wp_get_object_terms($post->ID, "author_posts");
             </div>
         </div>
 
-        <?php 
-        // Check if $autor is valid before using it
-        if (!is_wp_error($autor) && !empty($autor) && isset($autor[0]->term_id, $autor[0]->name)) : ?>
-            <a href="<?php echo esc_url(get_term_link($autor[0]->term_id)); ?>" class="post_author">
-                <?php echo esc_html($autor[0]->name); ?>
+        <?php if ($autor[0] && isset($autor[0]->term_id, $autor[0]->name)): ?>
+            <a href="<?php echo get_term_link($autor[0]->term_id) ?>" class="post_author">
+                <?php echo $autor[0]->name ?>
             </a>
-        <?php else : ?>
-            <span class="post_author">Unknown Author</span>
         <?php endif; ?>
 
         <a href="<?php the_permalink(); ?>" class="post_title">
-            <h1><?php the_title(); ?></h1>  
+            <h1><?php the_title(); ?></h1>
         </a>
 
-        <span class="post_date"> 
+        <span class="post_date">
             <?php media_am_localized_date(get_the_date('j F Y')); ?>
         </span>
     </div>
