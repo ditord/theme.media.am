f18b00c36d558069ec1945d592b31c17f9045470
Robert Papyan <robert.papyan@matemat.io>
2026-06-02 20:14:39 +0400

    Header redesign initial version

--- header.php
+++ header.php
@@ -7,13 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php bloginfo('name'); ?> | <?php
                                         if (is_home() == false) {
-                                            $title = return_lang('Մարդիկ են մեդիան | ', 'People are the media | ');
-                                            echo $title;
+                                            echo return_lang('Մարդիկ են մեդիան | ', 'People are the media | ');
                                             wp_title("");
                                         }
                                         if (is_home() == true) {
-                                            $title = return_lang('Մարդիկ են մեդիան', 'People are the media');
-                                            echo $title;
+                                            echo return_lang('Մարդիկ են մեդիան', 'People are the media');
                                         }
                                         ?></title>
     <link rel="shortcut icon" href="<?php full_path("images/media.am.ico") ?>" type="image/x-icon">
@@ -22,87 +20,82 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
-    <?php
-    $excerpt = get_the_excerpt()
-    ?>
-
+    <?php $excerpt = get_the_excerpt() ?>
     <?php wp_head() ?>
-
 </head>
 
 <body <?php body_class(); ?>>
     <div class="background-grey"></div>
+
     <nav class="header_menu">
-     
-        <?php
-        wp_nav_menu(array(
-            'theme_location' => 'primary'
-        ));
-        ?>
-        <div class="header_menu_logos">
-            <div class="header_project">
-                <span>
-                    <?php $project = return_lang('նախագիծը', 'project by');
-                    echo $project;
-                    ?>
-                </span>
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/logos/mic_logo_eng.svg"
-                    alt="MIC logo" width="84px" height="37px">
+        <div class="header_menu_inner">
+            <div class="header_menu_search">
+                <?php get_template_part('searchform', 'header'); ?>
             </div>
-            <div class="header_mediaethics">
-                <a href="https://mediaethics.am/media-outlets/media-am/" target="_blank">
-                    <img decoding="async" src="<?php echo get_template_directory_uri(); ?>/assets/logos/media_ethics_new.png" alt="Quality Sign BW">
-                </a>              
-            </div>
+
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'primary',
+                'container_class' => 'header_menu_grid',
+            ));
+            ?>
         </div>
     </nav>
+
     <header class="header">
         <nav class="header_nav">
-            <div class="header_flex header_flex1">
-                <a href="<?php echo esc_url(home_url('/')); ?>" class="header_logo">
-                    <?php
-                    $logo_srs = return_lang('images/logo_hy.png', 'images/logo_en.png') ?>
-                    <img src="<?php full_path($logo_srs) ?>" alt="">
-                </a>
-                <div class="header_logos">
-                    <div class="header_project">
-                        <span>
-                            <?php $project = return_lang('նախագիծը', 'project by');
-                            echo $project;
-                            ?>
-                        </span>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/logos/mic_logo_eng.svg"
-                            alt="MIC logo" width="84px" height="37px">
-                    </div>
-                    <div class="header_mediaethics">
-                        <a href="https://mediaethics.am/media-outlets/media-am/" target="_blank">
-                            <img decoding="async" src="<?php echo get_template_directory_uri(); ?>/assets/logos/media_ethics_new.png" alt="Quality Sign BW">
-
-                        </a>              
-                    </div>
-                </div>
-            </div>
-            <div class="header_flex header_flex2">
-                <?php do_action('media_am_language_switcher');?>
-                
-                <div class="header_search_container">
-                    <button class="header_open_search">
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/icons/search.svg"
-                            height="24px" width="24px">
-                        <span><?php lang('որոնում', 'search') ?></span>
-                    </button>
-                    <div class="header_search_form">
-                            <?php get_template_part( 'searchform','header'); ?>
-                    </div>    
-                </div>
-                
-                 
-                 
-                <button class="hamburger" type="button">
-                    <img class="header_burger_icon" src="<?php echo get_template_directory_uri(); ?>/assets/icons/menu_burger.svg" height="48px" width="48px">
-                    <img class="header_close_icon" src="<?php echo get_template_directory_uri(); ?>/assets/icons/close_menu.svg" height="48px" width="48px">
+            <div class="header_top">
+                <button class="hamburger" type="button" aria-label="<?php echo esc_attr(return_lang('Բացել մենյուն', 'Open menu')); ?>" aria-expanded="false">
+                    <img class="header_burger_icon" src="<?php echo esc_url(get_template_directory_uri() . '/assets/icons/menu-burger-with-search-icon.svg'); ?>" alt="">
+                    <img class="header_close_icon" src="<?php echo esc_url(get_template_directory_uri() . '/assets/icons/close_menu.svg'); ?>" alt="">
                 </button>
+
+                <a href="<?php echo esc_url(home_url('/')); ?>" class="header_logo">
+                    <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/logos/media.am_logo_new.svg'); ?>" alt="<?php bloginfo('name'); ?>">
+                </a>
+
+                <?php if (function_exists('icl_get_languages')) :
+                    $languages = icl_get_languages('skip_missing=0');
+                    $active_language = null;
+                    $other_languages = array();
+
+                    foreach ($languages as $language) {
+                        if (!empty($language['active'])) {
+                            $active_language = $language;
+                        } else {
+                            $other_languages[] = $language;
+                        }
+                    }
+
+                    $active_language_code = $active_language['language_code'] ?? $active_language['code'] ?? ICL_LANGUAGE_CODE;
+                    $active_language_label = strtolower($active_language_code) === 'hy' ? 'հայ' : 'eng';
+                    ?>
+                    <div class="header_language_dropdown">
+                        <button class="header_language_button" type="button" aria-expanded="false">
+                            <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/icons/language-switcher-icon.svg'); ?>" alt="">
+                            <span><?php echo esc_html($active_language_label); ?></span>
+                        </button>
+
+                        <?php if (!empty($other_languages)) : ?>
+                            <div class="header_language_options">
+                                <?php foreach ($other_languages as $language) : ?>
+                                    <a href="<?php echo esc_url($language['url']); ?>">
+                                        <?php echo esc_html($language['native_name']); ?>
+                                    </a>
+                                <?php endforeach; ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+
+            <div class="header_primary_menu">
+                <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'primary',
+                    'container_class' => 'header_primary_menu_container',
+                ));
+                ?>
             </div>
         </nav>
     </header>
-        
--- js/header.js
+++ js/header.js
@@ -1,70 +1,98 @@
 (function () {
   document.addEventListener('DOMContentLoaded', function () {
-      header_size();
+    const htmlElement = document.documentElement;
+    const header = document.querySelector('.header');
+    const headerMenu = document.querySelector('.header_menu');
+    const hamburger = document.querySelector('.hamburger');
+    const backgroundGrey = document.querySelector('.background-grey');
+    const languageDropdown = document.querySelector('.header_language_dropdown');
+    const languageButton = document.querySelector('.header_language_button');
 
-      // Toggle search form
-      document.querySelector('.header_open_search').addEventListener('click', function () {
-          document.querySelector('.header_search_form').classList.toggle('active');
-          document.querySelector('.header_search_container').classList.toggle('active');
-          document.querySelector('header > nav > div.header_flex .header_project').classList.toggle('hidden');
-          document.querySelector('header > nav > div.header_flex .header_mediaethics').classList.toggle('hidden');
-          const searchInput = document.querySelector('.header_search_form.active input[type="text"]');
-          if (searchInput) {
-              searchInput.focus();
-          }
+    updateHeaderState();
+    document.addEventListener('scroll', updateHeaderState, { passive: true });
+
+    if (hamburger) {
+      hamburger.addEventListener('click', toggleMenu);
+    }
+
+    if (backgroundGrey) {
+      backgroundGrey.addEventListener('click', closeMenu);
+    }
+
+    if (languageButton && languageDropdown) {
+      languageButton.addEventListener('click', function (event) {
+        event.stopPropagation();
+        languageDropdown.classList.toggle('active');
+        languageButton.setAttribute('aria-expanded', languageDropdown.classList.contains('active') ? 'true' : 'false');
       });
+    }
 
-      
-      document.querySelector('.close_search').addEventListener('click',function(e){
-            document.querySelector('header > nav > div.header_flex .header_project').classList.remove('hidden');
-            document.querySelector('header > nav > div.header_flex .header_mediaethics').classList.remove('hidden');
-            document.querySelector('.header_search_form').classList.remove('active');
-            document.querySelector('.header_search_container').classList.remove('active');
-      })
-      document.addEventListener('click', function (e) {
-          if (!e.target.closest('.header_search_form') && !e.target.closest('.header_open_search')) {
-              document.querySelector('header > nav > div.header_flex .header_project').classList.remove('hidden');
-              document.querySelector('header > nav > div.header_flex .header_mediaethics').classList.remove('hidden');
-              document.querySelector('.header_search_form').classList.remove('active');
-              document.querySelector('.header_search_container').classList.remove('active');
-          }
-      });
-
-      // Toggle menu and background overlay
-      document.querySelector('nav button.hamburger').addEventListener('click', toggleMenu);
-      document.querySelector('.background-grey').addEventListener('click', toggleMenu);
-  });
-
-  function toggleMenu() {
-    const hasScrollbar = window.innerWidth > document.documentElement.clientWidth;
-      const htmlElement = document.querySelector('html'); 
-      const header = document.querySelector('header');
-      document.querySelector('.header_menu').classList.toggle('active');
-      htmlElement.classList.toggle('no-scroll');
-     
-      if (hasScrollbar && htmlElement.classList.contains('no-scroll') ) {
-        html.style.paddingRight = '16px';
-        header.style.paddingRight = '24px';
-      }else{
-        html.style.paddingRight = '';
-        header.style.paddingRight = '';
+    document.addEventListener('click', function (event) {
+      if (languageDropdown && !event.target.closest('.header_language_dropdown')) {
+        languageDropdown.classList.remove('active');
+        if (languageButton) {
+          languageButton.setAttribute('aria-expanded', 'false');
+        }
       }
-      const backgroundGrey = document.querySelector('.background-grey');
-      if (backgroundGrey.style.display === 'block') {
-          backgroundGrey.style.display = 'none';
+    });
+
+    function updateHeaderState() {
+      if (!header) {
+        return;
+      }
+
+      if (window.scrollY > 0) {
+        header.classList.add('small');
       } else {
-          backgroundGrey.style.display = 'block';
+        header.classList.remove('small');
       }
-      document.querySelector('nav button.hamburger').classList.toggle('is-active');
-  }
+    }
 
-  function header_size() {
-      document.addEventListener('scroll', function () {
-          if (window.scrollY !== 0) {
-              document.querySelector('header').classList.add('small');
-          } else {
-              document.querySelector('header').classList.remove('small');
-          }
-      });
-  }
+    function toggleMenu() {
+      if (headerMenu && headerMenu.classList.contains('active')) {
+        closeMenu();
+      } else {
+        openMenu();
+      }
+    }
+
+    function openMenu() {
+      if (!headerMenu || !hamburger || !header) {
+        return;
+      }
+
+      headerMenu.classList.add('active');
+      header.classList.add('menu-open');
+      hamburger.classList.add('is-active');
+      hamburger.setAttribute('aria-expanded', 'true');
+      htmlElement.classList.add('no-scroll');
+
+      if (backgroundGrey) {
+        backgroundGrey.style.display = 'block';
+      }
+
+      const searchInput = headerMenu.querySelector('input[type="text"], input[type="search"]');
+      if (searchInput) {
+        window.setTimeout(function () {
+          searchInput.focus();
+        }, 100);
+      }
+    }
+
+    function closeMenu() {
+      if (!headerMenu || !hamburger || !header) {
+        return;
+      }
+
+      headerMenu.classList.remove('active');
+      header.classList.remove('menu-open');
+      hamburger.classList.remove('is-active');
+      hamburger.setAttribute('aria-expanded', 'false');
+      htmlElement.classList.remove('no-scroll');
+
+      if (backgroundGrey) {
+        backgroundGrey.style.display = 'none';
+      }
+    }
+  });
 })();
