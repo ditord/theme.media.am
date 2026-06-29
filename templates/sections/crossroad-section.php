3f5b93bede0939d9d6abb0b1ff91da0e001106db
Robert Papyan <robert.papyan@matemat.io>
2026-06-29 21:00:50 +0400

    changes

--- templates/sections/crossroad-section.php
+++ templates/sections/crossroad-section.php
@@ -26,6 +26,23 @@ if (!$crossroad_section_posts->have_posts()) {
 $crossroad_section_featured_post = isset($crossroad_section_posts->posts[0]) ? $crossroad_section_posts->posts[0] : null;
 $crossroad_section_small_post = isset($crossroad_section_posts->posts[1]) ? $crossroad_section_posts->posts[1] : null;
 $crossroad_section_bottom_posts = array_slice($crossroad_section_posts->posts, 2, 3);
+$crossroad_get_author = function ($post_id) {
+    $author_terms = wp_get_object_terms($post_id, 'author_posts');
+
+    if (is_wp_error($author_terms) || empty($author_terms)) {
+        return array(
+            'name' => '',
+            'link' => '',
+        );
+    }
+
+    $author_link = get_term_link($author_terms[0]);
+
+    return array(
+        'name' => $author_terms[0]->name,
+        'link' => is_wp_error($author_link) ? '' : $author_link,
+    );
+};
 ?>
 
 <section class="crossroad-section">
@@ -39,28 +56,26 @@ $crossroad_section_bottom_posts = array_slice($crossroad_section_posts->posts, 2
                 <?php
                 $post = $crossroad_section_small_post;
                 setup_postdata($post);
-                $crossroad_author = wp_get_object_terms(get_the_ID(), 'author_posts');
-                $crossroad_author_name = (!is_wp_error($crossroad_author) && !empty($crossroad_author)) ? $crossroad_author[0]->name : '';
-                $crossroad_author_link = (!is_wp_error($crossroad_author) && !empty($crossroad_author)) ? get_term_link($crossroad_author[0]) : '';
-                if (is_wp_error($crossroad_author_link)) {
-                    $crossroad_author_link = '';
-                }
+                $crossroad_author = $crossroad_get_author(get_the_ID());
                 ?>
-                <article class="crossroad-section__small-card">
-                    <a class="crossroad-section__category" href="<?php echo esc_url(get_category_link($crossroad_section_category)); ?>">
-                        <?php echo esc_html($crossroad_section_category->name); ?>
-                    </a>
-                    <a class="crossroad-section__small-title" href="<?php the_permalink(); ?>">
-                        <?php the_title(); ?>
-                    </a>
-                    <time class="crossroad-section__date" datetime="<?php echo esc_attr(get_the_date('c')); ?>">
-                        <?php media_am_localized_date(get_the_date('j F Y')); ?>
-                    </time>
-                    <?php if ($crossroad_author_name) : ?>
-                        <a class="viewpoint-post-block__author crossroad-section__author" href="<?php echo esc_url($crossroad_author_link); ?>">
-                            <span><?php echo esc_html($crossroad_author_name); ?></span>
-                        </a>
-                    <?php endif; ?>
+                <article class="crossroad-post crossroad-post--text">
+                    <div class="crossroad-post__content">
+                        <?php if ($crossroad_author['name']) : ?>
+                            <a class="crossroad-post__author crossroad-post__author--white" href="<?php echo esc_url($crossroad_author['link']); ?>">
+                                <?php echo esc_html($crossroad_author['name']); ?>
+                            </a>
+                        <?php endif; ?>
+
+                        <div class="crossroad-post__body">
+                            <a class="crossroad-post__title" href="<?php the_permalink(); ?>">
+                                <?php the_title(); ?>
+                            </a>
+
+                            <time class="crossroad-post__date" datetime="<?php echo esc_attr(get_the_date('c')); ?>">
+                                <?php media_am_localized_date(get_the_date('j F Y')); ?>
+                            </time>
+                        </div>
+                    </div>
                 </article>
             <?php endif; ?>
 
@@ -68,10 +83,33 @@ $crossroad_section_bottom_posts = array_slice($crossroad_section_posts->posts, 2
                 <?php
                 $post = $crossroad_section_featured_post;
                 setup_postdata($post);
-                $viewpoint_post_block_modifier = 'viewpoint-post-block--featured crossroad-section__featured';
-                include get_template_directory() . '/templates/post-blocks/viewpoint-post-block.php';
-                unset($viewpoint_post_block_modifier);
+                $crossroad_author = $crossroad_get_author(get_the_ID());
                 ?>
+                <article class="crossroad-post crossroad-post--wide">
+                    <a class="crossroad-post__image hover-image-scale" href="<?php the_permalink(); ?>">
+                        <?php if (has_post_thumbnail()) : ?>
+                            <?php the_post_thumbnail('large'); ?>
+                        <?php else : ?>
+                            <img src="<?php echo esc_url(get_template_directory_uri() . '/images/transparent_background.png'); ?>" alt="">
+                        <?php endif; ?>
+                    </a>
+
+                    <div class="crossroad-post__content">
+                        <?php if ($crossroad_author['name']) : ?>
+                            <a class="crossroad-post__author" href="<?php echo esc_url($crossroad_author['link']); ?>">
+                                <?php echo esc_html($crossroad_author['name']); ?>
+                            </a>
+                        <?php endif; ?>
+
+                        <a class="crossroad-post__title" href="<?php the_permalink(); ?>">
+                            <?php the_title(); ?>
+                        </a>
+
+                        <time class="crossroad-post__date" datetime="<?php echo esc_attr(get_the_date('c')); ?>">
+                            <?php media_am_localized_date(get_the_date('j F Y')); ?>
+                        </time>
+                    </div>
+                </article>
             <?php endif; ?>
         </div>
 
@@ -79,38 +117,35 @@ $crossroad_section_bottom_posts = array_slice($crossroad_section_posts->posts, 2
             <div class="crossroad-section__bottom-grid">
                 <?php foreach ($crossroad_section_bottom_posts as $post) :
                     setup_postdata($post);
-                    $crossroad_author = wp_get_object_terms(get_the_ID(), 'author_posts');
-                    $crossroad_author_name = (!is_wp_error($crossroad_author) && !empty($crossroad_author)) ? $crossroad_author[0]->name : '';
-                    $crossroad_author_link = (!is_wp_error($crossroad_author) && !empty($crossroad_author)) ? get_term_link($crossroad_author[0]) : '';
-                    if (is_wp_error($crossroad_author_link)) {
-                        $crossroad_author_link = '';
-                    }
+                    $crossroad_author = $crossroad_get_author(get_the_ID());
                 ?>
-                    <article class="plain-post-block crossroad-section__bottom-card">
-                        <a class="plain-post-block__image hover-image-scale" href="<?php the_permalink(); ?>">
-                            <?php if (has_post_thumbnail()) : ?>
-                                <?php the_post_thumbnail('large'); ?>
-                            <?php else : ?>
-                                <img src="<?php echo esc_url(get_template_directory_uri() . '/images/transparent_background.png'); ?>" alt="">
-                            <?php endif; ?>
-                        </a>
+                    <article class="crossroad-post crossroad-post--standard">
+                        <div class="crossroad-post__media">
+                            <a class="crossroad-post__image hover-image-scale" href="<?php the_permalink(); ?>">
+                                <?php if (has_post_thumbnail()) : ?>
+                                    <?php the_post_thumbnail('large'); ?>
+                                <?php else : ?>
+                                    <img src="<?php echo esc_url(get_template_directory_uri() . '/images/transparent_background.png'); ?>" alt="">
+                                <?php endif; ?>
+                            </a>
 
-                        <a class="plain-post-block__tag" href="<?php echo esc_url(get_category_link($crossroad_section_category)); ?>">
-                            <?php echo esc_html($crossroad_section_category->name); ?>
-                        </a>
+                            <a class="crossroad-post__tag" href="<?php echo esc_url(get_category_link($crossroad_section_category)); ?>">
+                                <?php echo esc_html($crossroad_section_category->name); ?>
+                            </a>
+                        </div>
 
-                        <div class="plain-post-block__content">
-                            <?php if ($crossroad_author_name) : ?>
-                                <a class="viewpoint-post-block__author crossroad-section__author" href="<?php echo esc_url($crossroad_author_link); ?>">
-                                    <span><?php echo esc_html($crossroad_author_name); ?></span>
+                        <div class="crossroad-post__content">
+                            <?php if ($crossroad_author['name']) : ?>
+                                <a class="crossroad-post__author" href="<?php echo esc_url($crossroad_author['link']); ?>">
+                                    <?php echo esc_html($crossroad_author['name']); ?>
                                 </a>
                             <?php endif; ?>
 
-                            <a class="plain-post-block__title" href="<?php the_permalink(); ?>">
+                            <a class="crossroad-post__title" href="<?php the_permalink(); ?>">
                                 <?php the_title(); ?>
                             </a>
 
-                            <time class="plain-post-block__date" datetime="<?php echo esc_attr(get_the_date('c')); ?>">
+                            <time class="crossroad-post__date" datetime="<?php echo esc_attr(get_the_date('c')); ?>">
                                 <?php media_am_localized_date(get_the_date('j F Y')); ?>
                             </time>
                         </div>
@@ -123,5 +158,5 @@ $crossroad_section_bottom_posts = array_slice($crossroad_section_posts->posts, 2
 
 <?php
 wp_reset_postdata();
-unset($crossroad_section_category_slug, $crossroad_section_posts_count, $crossroad_section_category, $crossroad_section_posts, $crossroad_section_featured_post, $crossroad_section_small_post, $crossroad_section_bottom_posts, $crossroad_author, $crossroad_author_name, $crossroad_author_link);
+unset($crossroad_section_category_slug, $crossroad_section_posts_count, $crossroad_section_category, $crossroad_section_posts, $crossroad_section_featured_post, $crossroad_section_small_post, $crossroad_section_bottom_posts, $crossroad_get_author, $crossroad_author);
 ?>
